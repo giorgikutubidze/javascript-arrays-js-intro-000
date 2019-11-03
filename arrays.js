34ec4aabd1eddded8698de3ceb5495e2cf6c9d91
giorgi <giorgikutubidze@gmail.com>
2019-11-03 19:43:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array,element){
   return newarray;
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
-  array = [element,...array];
+  array.unshift(element);
   return array;
 }
 function addElementToEndOfArray(array,element){
